f5b757006b62f8ed1fb4d3ec367b6f80c5fa9049
Quentin <kheir2322@gmail.com>
2025-02-19 08:52:48 +0100

    Programme pour faire parler le robot

--- nao_speak.py
+++ nao_speak.py
@@ -2,13 +2,10 @@
 
 from naoqi import ALProxy
 
-# Adresse IP du robot NAO
 if __name__ == "__main__":
-    NAO_IP = "192.168.143.30"  # Remplace par l'IP du NAO
+    NAO_IP = "192.168.143.30" 
     NAO_PORT = 9559
 
-# Connexion au module de synthèse vocale
 tts = ALProxy("ALTextToSpeech", NAO_IP, NAO_PORT)
 tts.setLanguage("French")
-# Faire parler le robot
 tts.say("Bonjour, je suis NAO !")
